a2bc237616bc793c67adb12426a22a44b32f0f26
Gleidson Neves <gleidisonmt@gmail.com>
2026-03-19 17:55:18 -0300

    # 14 - #8 Delete option task will call i18n text.

--- app/src/main/java/io/github/gleidsonmt/todo/view/panel/menu/task_menu_items/MenuItemDelete.java
+++ app/src/main/java/io/github/gleidsonmt/todo/view/panel/menu/task_menu_items/MenuItemDelete.java
@@ -2,6 +2,7 @@ package io.github.gleidsonmt.todo.view.panel.menu.task_menu_items;
 
 import io.github.gleidsonmt.glad.controls.icon.Icon;
 import io.github.gleidsonmt.glad.controls.icon.SVGIcon;
+import io.github.gleidsonmt.todo.utils.I18n;
 import io.github.gleidsonmt.todo.view.panel.items.TaskItemViewModel;
 import javafx.scene.control.MenuItem;
 import javafx.scene.input.KeyCode;
@@ -23,7 +24,7 @@ public class MenuItemDelete extends MenuItem {
         this.setAccelerator(new KeyCodeCombination(KeyCode.DELETE));
         this.getStyleClass().addAll("menu-item-last", "menu-item-delete");
         this.setGraphic(new SVGIcon(Icon.DELETE));
-        this.setText("Delete Task");
+        this.setText(I18n.get("menu.delete.task"));
 
         this.setOnAction(e -> {
             viewModel.delete();
