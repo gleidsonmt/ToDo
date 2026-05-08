456db9f0f409dea9777b9e8726de0c19b4fec9fb
Gleidson Neves <gleidisonmt@gmail.com>
2026-05-08 14:13:17 -0300

    refactor bd script

--- app/src/main/resources/io/github/gleidsonmt/todo/sql/bd_script.sql
+++ app/src/main/resources/io/github/gleidsonmt/todo/sql/bd_script.sql
@@ -1,81 +1,73 @@
--- --------------------------------------------------------
--- Servidor:                     127.0.0.1
--- Versão do servidor:           8.0.16 - MySQL Community Server - GPL
--- OS do Servidor:               Win64
--- HeidiSQL Versão:              12.9.0.6999
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40103 SET @OLD_TIME_ZONE = @@TIME_ZONE */;
+/*!40103 SET TIME_ZONE = '+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
+/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
 
 
--- Copiando estrutura do banco de dados para todo
 DROP DATABASE IF EXISTS `todo`;
-CREATE DATABASE IF NOT EXISTS `todo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `todo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
 USE `todo`;
 
--- Copiando estrutura para tabela todo.list
 DROP TABLE IF EXISTS `list`;
-CREATE TABLE IF NOT EXISTS `list` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  `fixed` tinyint(4) DEFAULT NULL,
-  `icon_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `size` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `list`
+(
+    `id`        int(11) NOT NULL AUTO_INCREMENT,
+    `name`      varchar(50)                                                  DEFAULT NULL,
+    `fixed`     tinyint(4)                                                   DEFAULT NULL,
+    `icon_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+    `size`      int(11)                                                      DEFAULT '0',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
--- Copiando dados para a tabela todo.list: ~6 rows (aproximadamente)
-INSERT INTO `list` (`id`, `name`, `fixed`, `icon_name`, `size`) VALUES
-	(0, 'tasks', 1, 'home', 0),
-	(1, 'my_day', 1, 'sun', 2),
-	(2, 'important', 1, 'star', 1),
-	(3, 'Dashboard', 0, 'notebook', 1),
-	(4, 'To Buy', 0, 'shopping', 0),
-	(5, 'Exercise', 0, 'strong-arm', 0);
+INSERT INTO `list` (`id`, `name`, `fixed`, `icon_name`, `size`)
+VALUES (1, 'tasks', 1, 'home', 0),
+       (2, 'my_day', 1, 'sun', 0),
+       (3, 'important', 1, 'star', 0),
+       (4, 'Dashboard', 0, 'notebook', 0),
+       (5, 'To Buy', 0, 'shopping', 0),
+       (6, 'Exercise', 0, 'strong-arm', 0);
 
--- Copiando estrutura para tabela todo.recurrence
-DROP TABLE IF EXISTS `recurrence`;
-CREATE TABLE IF NOT EXISTS `recurrence` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `gap` int(11) DEFAULT NULL,
-  `type` enum('DAILY','WEEKLY','MONTHLY','YEARLY') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'DAILY',
-  `days_of_week` set('SUNDAY','MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','ANY') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'ANY',
-  `task_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_recurrence_task` (`task_id`),
-  CONSTRAINT `FK_recurrence_task` FOREIGN KEY (`task_id`) REFERENCES `task` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS `task`
+(
+    `id`         int(11) NOT NULL AUTO_INCREMENT,
+    `name`       varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+    `completed`  tinyint(4)                                                   DEFAULT NULL,
+    `important`  tinyint(4)                                                   DEFAULT NULL,
+    `my_day`     tinyint(4)                                                   DEFAULT NULL,
+    `due_date`   date                                                         DEFAULT NULL,
+    `remind`     datetime                                                     DEFAULT NULL,
+    `created_at` date                                                         DEFAULT NULL,
+    `list_id`    int(11) NOT NULL                                             DEFAULT '0',
+    PRIMARY KEY (`id`) USING BTREE,
+    KEY `FK_task_list` (`list_id`),
+    CONSTRAINT `FK_task_list` FOREIGN KEY (`list_id`) REFERENCES `list` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci
+  ROW_FORMAT = DYNAMIC;
 
--- Copiando dados para a tabela todo.recurrence: ~0 rows (aproximadamente)
+CREATE TABLE IF NOT EXISTS `recurrence`
+(
+    `id`           int(11) NOT NULL AUTO_INCREMENT,
+    `gap`          int(11)                                                                                                                             DEFAULT NULL,
+    `type`         enum ('DAILY','WEEKLY','MONTHLY','YEARLY') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci                                         DEFAULT 'DAILY',
+    `days_of_week` set ('SUNDAY','MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY','SATURDAY','ANY') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'ANY',
+    `task_id`      int(11)                                                                                                                             DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `FK_recurrence_task` (`task_id`),
+    CONSTRAINT `FK_recurrence_task` FOREIGN KEY (`task_id`) REFERENCES `task` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
--- Copiando estrutura para tabela todo.task
-DROP TABLE IF EXISTS `task`;
-CREATE TABLE IF NOT EXISTS `task` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `completed` tinyint(4) DEFAULT NULL,
-  `important` tinyint(4) DEFAULT NULL,
-  `my_day` tinyint(4) DEFAULT NULL,
-  `due_date` date DEFAULT NULL,
-  `remind` datetime DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  `list_id` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`) USING BTREE,
-  KEY `FK_task_list` (`list_id`),
-  CONSTRAINT `FK_task_list` FOREIGN KEY (`list_id`) REFERENCES `list` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
-
--- Copiando dados para a tabela todo.task: ~0 rows (aproximadamente)
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40103 SET TIME_ZONE = IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE = IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS = IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES = IFNULL(@OLD_SQL_NOTES, 1) */;
